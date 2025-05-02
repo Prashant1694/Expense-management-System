58149f1cffd72895bfb92072f9a03610f710e4ee
Prashant <141180383+Prashant1694@users.noreply.github.com>
2025-05-03 02:07:36 +0530

    Update Spinner.js

--- client/src/components/layouts/Spinner.js
+++ client/src/components/layouts/Spinner.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Spinner = () => {
   return (
-    <><button class="btn btn-primary" type="button" disabled>
-              <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-              <span role="status">Please Wait</span>
-          </button></>
-  )
-}
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-75">
+      <div className="flex flex-col items-center space-y-2">
+        <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full" role="status" />
+        <span className="text-blue-700 text-lg font-medium">Please wait...</span>
+      </div>
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
