16b2d57c371b297c31d482f56602361be7cb3097
Prashant <141180383+Prashant1694@users.noreply.github.com>
2025-05-03 02:03:39 +0530

    Update Spinner.js

--- client/src/components/layouts/Spinner.js
+++ client/src/components/layouts/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <></button><button class="btn btn-primary" type="button" disabled>
+    <><button class="btn btn-primary" type="button" disabled>
               <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
               <span role="status">Please Wait</span>
           </button></>
